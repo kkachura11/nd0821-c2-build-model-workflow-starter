3eb4959aed785efcc48cdf0c65014a8111a3d27d
Kseniia <ksenija.kachura@gmail.com>
2022-03-12 10:50:43 +0100

    Drop outliers in geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,7 +27,7 @@ def go(args):
     df = df[idx].copy()
 
     logger.info("Drop the outliers in geolocation")
-    dx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
     logger.info("Save results")
